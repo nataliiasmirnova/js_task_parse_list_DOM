9d44826e4617e5c690655031dcf900ad09268a86
Nataliia Smirnova <ensueno11@gmail.com>
2024-11-27 23:30:51 +0200

    feat: task implementation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+// Working with data
+// Making array from tag nodes
+const dataList = Array.from(document.querySelectorAll('li'));
+
+//  Function to convert USD to numbers
+function convertation(arg) {
+  return parseFloat(arg.replace(/[$,]/g, ''));
+}
+
+// Getting sorted list
+const sortedItems = dataList.sort((a, b) => {
+  const salaryA = convertation(a.dataset.salary);
+  const salaryB = convertation(b.dataset.salary);
+
+  return salaryB - salaryA;
+});
+
+// Reset the main list
+const list = document.querySelector('ul');
+
+sortedItems.forEach((item) => list.appendChild(item));
