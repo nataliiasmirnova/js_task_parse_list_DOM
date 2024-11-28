3866951994df25dfb588f3d6d6b8a740dfbdc113
Nataliia Smirnova <ensueno11@gmail.com>
2024-11-28 14:45:35 +0200

    fix: function getEmployees and sortList added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,15 +9,44 @@ function convertation(arg) {
   return parseFloat(arg.replace(/[$,]/g, ''));
 }
 
-// Getting sorted list
-const sortedItems = dataList.sort((a, b) => {
-  const salaryA = convertation(a.dataset.salary);
-  const salaryB = convertation(b.dataset.salary);
+// Function getEmployees
+function getEmployeesConvertation(data) {
+  const listOfEmployees = dataList.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.dataset.position,
+    salary: item.dataset.salary,
+    age: +item.dataset.age,
+  }));
 
-  return salaryB - salaryA;
-});
+  return listOfEmployees;
+}
+
+const list = getEmployeesConvertation(dataList);
+
+// Getting sorted list
+function sortList(dataToSort) {
+  return dataToSort.sort((a, b) => {
+    const salaryA = convertation(a.salary);
+    const salaryB = convertation(b.salary);
+
+    return salaryB - salaryA;
+  });
+}
+
+const sortedItems = sortList(list);
 
 // Reset the main list
-const list = document.querySelector('ul');
+const originalList = document.querySelector('ul');
 
-sortedItems.forEach((item) => list.appendChild(item));
+originalList.innerHTML = '';
+
+// Create new sorted list
+sortedItems.forEach((item) => {
+  const li = document.createElement('li');
+
+  li.textContent = `${item.name}`;
+  li.dataset.position = `${item.age}`;
+  li.dataset.salary = `${item.salary}`;
+  li.dataset.age = `${item.age}`;
+  originalList.appendChild(li);
+});
